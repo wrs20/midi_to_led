5466a17b6ed6e340911d0642c5150352b1c4c95c
William Saunders <wrs20@bath.ac.uk>
2018-12-26 16:36:28 +0000

    realtime progress

--- midi_to_led/realtime.py
+++ midi_to_led/realtime.py
@@ -1,14 +1,30 @@
 import mido
+from led import Led
 
 
 
+class RealtimeLed:
+    def __init__(self, ledset):
+        self.ledset = ledset
+        self._notes_on = {}
 
+    def _ledrefresh(self):
+        print(self._notes_on)
+    
+    def __call__(self, msg):
+        print(msg)
+        if msg.type == 'note_on':
+            self._notes_on[msg.note] = msg.velocity
+        
+        elif msg.type == 'note_off':
+            del self._notes_on[msg.note]
 
-
+        self._ledrefresh()
 
 
 
 if __name__ == '__main__':
+    # get input port
     devs = mido.get_input_names()
     dev = None
     for dx in devs:
@@ -20,20 +36,27 @@ if __name__ == '__main__':
     print(inport)
 
 
-
+    # get output port
     devs = mido.get_output_names()
-    print(devs)
     dev = None
     for dx in devs:
         if dx.startswith('FLUID'):
             dev = dx
             break
     assert dev is not None, "Could not find a fluidsynth output"
-
     outport = mido.open_output(dev)
+    print(outport)
+
+    
+    # make led rendering objects
+    ledset = Led(5)
+    rtled = RealtimeLed(ledset)
+
+
+
 
     for msg in inport:
-        print(msg)
+        rtled(msg)
         outport.send(msg)
 
 
