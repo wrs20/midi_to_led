e400c00ec01c5ac755d17ad813df883f6d1d5af9
wrs <wrs@wrsaunders.co.uk>
2018-12-26 16:01:46 +0000

    basic keyboard to fluidsynth

--- midi_to_led/realtime.py
+++ midi_to_led/realtime.py
@@ -18,7 +18,24 @@ if __name__ == '__main__':
     assert dev is not None, "Could not find LPK25."
     inport = mido.open_input(dev)
     print(inport)
+
+
+
+    devs = mido.get_output_names()
+    print(devs)
+    dev = None
+    for dx in devs:
+        if dx.startswith('FLUID'):
+            dev = dx
+            break
+    assert dev is not None, "Could not find a fluidsynth output"
+
+    outport = mido.open_output(dev)
+
     for msg in inport:
         print(msg)
+        outport.send(msg)
+
+
 
 
