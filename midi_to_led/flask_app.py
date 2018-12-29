018c2a799967ad4a88e4d6a00c3340ba800348c0
William Saunders <wrs20@bath.ac.uk>
2018-12-29 17:00:00 +0000

    arrrg opencv is silly

--- midi_to_led/flask_app.py
+++ midi_to_led/flask_app.py
@@ -1,57 +1,52 @@
 from flask import Flask, render_template, request
+from led import Led
 
-from subprocess import PIPE, Popen
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-def get_uptime():
-    return check_output_wrapper(('uptime',))
 
-def check_output_wrapper(cmd):
-    r = ''
-    rc = None
-    try:
-        p = Popen(cmd, stdout=PIPE, stderr=PIPE)
-        rc = p.communicate()
-    except Exception as e:
-        r += str(e)
-        if rc is not None:
-            r += rc[0].decode('utf-8') + rc[1].decode('utf-8')
-
-    if p.returncode != 0:
-        r = str(p.returncode) + '\n' + rc[0].decode('utf-8') + '\n' + rc[1].decode('utf-8')
-    elif rc is not None:
-        r = rc[0].decode('utf-8')
-    else:
-        r = 'Something is broken.'
-    return r
-
-# assumes the following in /etc/sudoers
-"""
-pi ALL=NOPASSWD: /sbin/halt, /sbin/reboot, /sbin/poweroff
-"""
 
 curr_keys = (
-    'rr0',
-    'gg0',
-    'bb0',
-    'br0'
+    'rr0', 'gg0', 'bb0', 'br0',
+    'rr1', 'gg1', 'bb1', 'br1',
+    'rr2', 'gg2', 'bb2', 'br2',
+    'rr3', 'gg3', 'bb3', 'br3',
+    'rr4', 'gg4', 'bb4', 'br4',
 )
 
+
+
+
+
 curr_vals = (
-    '128',
-    '128',
-    '128',
-    '50'
+    '0', '0', '0', '0',
+    '0', '0', '0', '0',
+    '0', '0', '0', '0',
+    '0', '0', '0', '0',
+    '0', '0', '0', '0',
 )
 
 
 curr_state = dict()
-for kx, vx in zip(curr_keys, curr_vals):
-    curr_state[kx] = vx
 
-print(curr_state)
+def reset_state():
+    for kx, vx in zip(curr_keys, curr_vals):
+        curr_state[kx] = vx
+
+reset_state()
+
+globalled = Led(5)
+def update_leds():
+    globalled.set(4, int(curr_state['rr0']), int(curr_state['gg0']), int(curr_state['bb0']), int(curr_state['br0']))
+    globalled.set(3, int(curr_state['rr1']), int(curr_state['gg1']), int(curr_state['bb1']), int(curr_state['br1']))
+    globalled.set(2, int(curr_state['rr2']), int(curr_state['gg2']), int(curr_state['bb2']), int(curr_state['br2']))
+    globalled.set(1, int(curr_state['rr3']), int(curr_state['gg3']), int(curr_state['bb3']), int(curr_state['br3']))
+    globalled.set(0, int(curr_state['rr4']), int(curr_state['gg4']), int(curr_state['bb4']), int(curr_state['br4']))
+
+update_leds()
+
+
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -61,23 +56,14 @@ def index():
         print(request)
         print(request.form)
         
-        for kx in curr_keys:
-            curr_state[kx] = request.form[kx]
-        
-        print(curr_state)
+        if request.form.get('reset_leds') == 'Reset':
+            reset_state()
 
-        #if request.form.get('clear_print_queue') == 'Clear Print Queue':
-        #    # pass
-        #    print("Clear Queue")
-        #    stdout = 'Error: not implemented.'
-        #elif  request.form.get('reboot_pi') == 'Reboot Pi':
-        #    print("Reboot pi")
-        #    # assuming that this user can execute 'sudo reboot' without password
-        #    stdout = check_output_wrapper(('sudo', 'reboot'),)
-        #elif  request.form.get('shutdown_pi') == 'Shutdown Pi':
-        #    # assuming that this user can execute 'sudo poweroff' without password
-        #    print("shutdown pi")
-        #    stdout = check_output_wrapper(('sudo', 'poweroff'),)
+        elif request.form.get('update_leds') == 'Update LEDs':
+            for kx in curr_keys:
+                curr_state[kx] = request.form[kx]
+
+        update_leds()
 
         return render_template("index.html", **curr_state)
 
